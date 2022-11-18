5d7519c9890d418d1575bf6d2316b84ff077c04d
David Mo <44306450+unknown@users.noreply.github.com>
2022-11-18 00:02:40 -0500

    Add basic hero

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,25 +3,41 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `bugs-website`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
     },
-    __key: "images"
-  }]
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /svgs/,
+        },
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
 
 export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,32 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import Button from "../components/Button";
+import NavBar from "../components/NavBar";
+import BugsLogo from "../svgs/bugs.svg";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </main>
+    <div className="min-h-screen antialiased">
+      <NavBar />
+      <main className="mx-auto max-w-5xl px-8">
+        <div className="flex space-x-32 items-center">
+          <div className="mt-24">
+            <h1 className="text-8xl font-bold">BUGS @ NYU</h1>
+            <p className="mt-6">
+              NYU’s premier open source club connecting students to create an
+              inclusive environment to work on projects together.
+            </p>
+            <div className="mt-6 flex space-x-4">
+              <Button variant="primary">Join the Club</Button>
+              <Button>Learn More</Button>
+            </div>
+          </div>
+          <div className="hidden md:block">
+            <BugsLogo />
+          </div>
+        </div>
+      </main>
+    </div>
   );
 };
 
