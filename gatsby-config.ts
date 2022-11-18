f4f81225b3b90ad57efcda5ce0447e7c6d0f82af
calvinatian <48587962+calvinatian@users.noreply.github.com>
2022-11-18 11:45:25 -0500

    ADD gh-pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: "/bugs-website",
   siteMetadata: {
     title: `bugs-website`,
     siteUrl: `https://www.yourdomain.tld`,
