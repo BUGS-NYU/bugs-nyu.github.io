9fc53fb6a65ab2999a1f3b490d51cfc2e7485b3d
Julius <45975811+nedjulius@users.noreply.github.com>
2022-11-18 21:00:57 -0500

    Add eslint, prettier and husky pre-commit hook (#6)
    * Add eslint and prettier configurations, format code
    
    * Add husky pre-commit and pre-push hooks
    
    * Remove npm test from husky pre-commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,42 +1,42 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  pathPrefix: "/bugs-website",
+  pathPrefix: '/bugs-website',
   siteMetadata: {
-    title: `bugs-website`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: 'bugs-website',
+    siteUrl: 'https://www.yourdomain.tld',
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
     {
-      resolve: "gatsby-plugin-react-svg",
+      resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
           include: /svgs/,
         },
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,39 +1,37 @@
-import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
-import Button from "../components/Button";
-import NavBar from "../components/NavBar";
-import BugsLogo from "../svgs/bugs.svg";
-import Card from "../components/Card";
+import * as React from 'react';
+import type { HeadFC, PageProps } from 'gatsby';
+import Button from '../components/Button';
+import NavBar from '../components/NavBar';
+import BugsLogo from '../svgs/bugs.svg';
+import Card from '../components/Card';
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <div className="min-h-screen antialiased">
+    <div className='min-h-screen antialiased'>
       <NavBar />
-      <main className="mx-auto max-w-5xl px-8">
-        <h2 className="mt-2 text-4xl font-bold">Projects</h2>
-        <p className="mt-2">
-          BUGS is meant to be a collaborative and inclusive environment for
-          everyone. We’re always welcome to hear your ideas and work on new
-          projects. Here are some past projects we have worked on.
+      <main className='mx-auto max-w-5xl px-8'>
+        <h2 className='mt-2 text-4xl font-bold'>Projects</h2>
+        <p className='mt-2'>
+          BUGS is meant to be a collaborative and inclusive environment for everyone. We’re always
+          welcome to hear your ideas and work on new projects. Here are some past projects we have
+          worked on.
         </p>
-        <div className="mt-8 flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
-          <Card shadow="normal">
-            <h3 className="font-bold">Schedge API</h3>
-            <p className="mt-2">
-              Schedge is an open source API to NYU's course catalog, geared at
-              eventually helping NYU students plan their courses easier and
-              faster. Our goal with this API is to make it easier for students
-              to plan out their schedules, and eventually to also do some of
+        <div className='mt-8 flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4'>
+          <Card shadow='normal'>
+            <h3 className='font-bold'>Schedge API</h3>
+            <p className='mt-2'>
+              Schedge is an open source API to NYU's course catalog, geared at eventually helping
+              NYU students plan their courses easier and faster. Our goal with this API is to make
+              it easier for students to plan out their schedules, and eventually to also do some of
               that for them
             </p>
           </Card>
-          <Card shadow="normal">
-            <h3 className="font-bold">Schedge API</h3>
-            <p className="mt-2">
-              Schedge is an open source API to NYU's course catalog, geared at
-              eventually helping NYU students plan their courses easier and
-              faster. Our goal with this API is to make it easier for students
-              to plan out their schedules, and eventually to also do some of
+          <Card shadow='normal'>
+            <h3 className='font-bold'>Schedge API</h3>
+            <p className='mt-2'>
+              Schedge is an open source API to NYU's course catalog, geared at eventually helping
+              NYU students plan their courses easier and faster. Our goal with this API is to make
+              it easier for students to plan out their schedules, and eventually to also do some of
               that for them
             </p>
           </Card>
