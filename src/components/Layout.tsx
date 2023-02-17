43ca2e8ccea6c62f9c6dc4021c0dd24753f22510
David Mo <44306450+unknown@users.noreply.github.com>
2023-02-17 13:48:52 -0500

    Add vertical padding to default layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ import Footer from './Footer';
 const Layout: React.FC<{ children: JSX.Element | JSX.Element[] }> = ({ children }) => (
   <div className='min-h-screen antialiased'>
     <NavBar />
-    <main className='mx-auto min-h-[calc(100vh-12rem)] max-w-5xl px-8'>{children}</main>
+    <main className='mx-auto min-h-[calc(100vh-12rem)] max-w-5xl p-8'>{children}</main>
     <Footer />
   </div>
 );
