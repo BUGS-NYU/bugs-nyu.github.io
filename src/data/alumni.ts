95e7376694096717f441ac04d673d1a614315210
aminoa <28660350+aminoa@users.noreply.github.com>
2023-03-13 23:49:47 -0400

    set up data

---
+++ src/data/alumni.ts
@@ -0,0 +1,127 @@
+export interface Alumni {
+  name: string;
+  description: string;
+}
+
+export const team: Alumni[] = [
+  {
+    name: 'Khanh Nguyen:',
+    description: `Khanh is a Junior from Vietnam studying Computer Science. BROCKHAMPTON is his all-time favorite boyband. Outside of school, he enjoys trying out good food, exploring the city and contributing to projects. 
+    Khanh was the former president.`,
+  },
+  {
+    name: 'Aaditya Mehta',
+    description: `Aaditya is a Senior studying Economics and Mathematics, Aaditya spends way too much time in Stern (#IAmStern). In his free time he likes to listen to music, take long walks, and read Econ papers, sometimes simultaneously. 
+    Aaditya was the former treasurer.`,
+  },
+  {
+    name: 'Sonia Jain',
+    description: `I'm Sonia and I'm a junior at New York University double majoring in Computer Science and Media, Culture, and Communications. I am interested in game design, art, and I love animals. 
+    In my free time, I work with a variety of organizations to help find dogs and cats' homes. I also love painting and digital art so when I'm not working with animals, I am creating. Sonia was the former director of marketing & communications.`,
+  },
+  {
+    name: 'Bruce Jiang',
+    description: `Bruce is a junior from China studying Computer Science and Mathematics. Outside of class, he is crazy in soccer (especially in Chelsea) and basketball. He spends tons of time playing FIFA (most of the time playing Chelsea). 
+    Besides, he likes listening to music, reading about history, and travelling. He was the former secretary.`,
+  },
+  {
+    name: 'Evan Silverman',
+    description:
+      'Evan is a Junior studying CS at CAS. In his free time he enjoys working on too many side projects, expanding his record collection, and making memes for @nyumsfbt. He was a former full stack developer.',
+  },
+  {
+    name: 'Yuchen Liu',
+    description:
+      'Yuchen is a CS and Data Science major at CAS. He likes to eat ramen, watch movies, meet new people, and work on fun projects. He was a former full stack developer.',
+  },
+  {
+    name: 'Samuel Lin',
+    description: `Samuel is a Senior at CAS majoring in Computer Science and minoring in Web Design and Game Design. He was born and raised in Taiwan, but studied in Singapore for high school before coming to New York. 
+    He is a big foodie and enjoys trying foods from different cultures. He was a former event coordinator.`,
+  },
+  {
+    name: 'Vishal Vanam',
+    description:
+      'I am a Graduate Student in Information Systems at GSAS. I hail from Hyderabad(India) and love travelling and exploring new places. Usually spotted listening to music and engaging in debates with friends. He was a former event coordinator.',
+  },
+  {
+    name: 'Nadira Dewji',
+    description:
+      'Nadira was one of the Founders of BUGS. An alumni studied Computer Science in CAS. Originally from Tanzania, Nadira loves travelling, scuba diving and wildlife. She’s excited about open source principles and making impactful contributions.',
+  },
+  {
+    name: 'David Klein',
+    description:
+      'David was one of the Founders of BUGS. Alumni from California who studied Biology with three minors, and loves both carbon based life and silicon based life to equal extents.',
+  },
+  {
+    name: 'Ananditha Raghunath',
+    description: `Ananditha was one of the Founders of BUGS. Alunmi who studied Math and CS at Courant. Having started to code only after she arrived at NYU, she is excited to mentor new members that may not know how to make their ideas translate into 
+    tangible code based projects!`,
+  },
+  {
+    name: 'Veena Arvind',
+    description:
+      "Veena was BUGS's predident from 2018 to 2020. Alumni who studied Computer Science in CAS. She loves to talk about code with other people. In her free time, Veena goes to the gym, makes jewelry, and plays card games. She's a SWE at Google.",
+  },
+  {
+    name: 'Shelly Cao',
+    description: `Shelly Cao was BUGS's Director of Outreach in 2019. Alumni who majored in Computer Science with minors in Math and Business Study. She comes from a small city in China called Jingzhou and even her Chinese friends are clueless as to where it is. 
+    In her free time, she loves to explore the different boroughs in the city and try food from different cultures.`,
+  },
+  {
+    name: 'Julie Cestaro',
+    description:
+      'Alumni from Connecticut who majored in Computer Science and minored in Business Studies and Math. You may also find her on campus doing work for CAS Student Council.',
+  },
+  {
+    name: 'George Ma',
+    description:
+      'Alumni from Singapore who majored in Computer Science and Business. He likes reading, writing, coding, and gaming.',
+  },
+  {
+    name: 'Deanna Tran',
+    description:
+      "Deanna was BUGS's Director of Communication in 2019. Alumni from CAS who studied Computer Science with minors in Web Design and Linguistics. Outside of school, Deanna can be found at a local restaurant or art museum. She's a SWE at Ellevest.",
+  },
+  {
+    name: 'Candice Shen',
+    description: `Candice Shen was BUGS's Director of Communications in 2019. Alumni from Steinhardt who majored in Media, Culture, and Communication and minored in Web Design. If you cannot find her outside of class, 
+    Candice is traveling or planning for her 23rd country to visit.`,
+  },
+  {
+    name: 'Bradley Brecher',
+    description:
+      "Alumni from Woodmere, New York who studied Computer Science. Outside of BUGS, you can find him coaching hockey at a local high school. He's a Backend Programmer at SomethingDigital.",
+  },
+  {
+    name: 'Ami Nachiappan',
+    description:
+      "Ami was BUGS's Event Coordinator from 2018 to 2020. Alumni from California who studied Computer Science and minored in Web Design. In her free time, she loves spending time with family, running, and of course, coding!",
+  },
+  {
+    name: 'Brian Shih',
+    description: `Brian was BUGS's webmaster from 2018 to 2020. Alumni from CAS who majored in Computer Science and minored in Cyber Security, Web Development, & Studio Art. Outside of class you can find him working a variety of things including custom IOT devices, 
+    simple CNN for puzzle solving, home automation programs, and building mobile/web apps. He's a SWE at NextJump.`,
+  },
+  {
+    name: 'Albert Liu',
+    description: `Albert was BUGS's Vice President. Alumni from CAS who majored in Computer Science. He has worked on Schedge, an API for NYU course data, as well as HumanToHuman, an application to help researchers study human interaction patterns. 
+    In his free time he enjoys going on long walks and taking longer naps.`,
+  },
+  {
+    name: 'Jiayue (Joanna) Zhang',
+    description:
+      "Joanna was BUGS's Director of Marketing. Alumni from CAS who majored in Mathematics and Computer Science. She enjoys working on machine learning research, supporting local restaurants, capturing NYC through her lens, and taking a walk at sunset.",
+  },
+  {
+    name: 'Edmond Shao',
+    description:
+      "Edmond was BUGS's Director of Communications. Alumni from CAS who majored in Computer Science and minored in Business Studies. He likes cars, being a foodie and traveling.",
+  },
+  {
+    name: 'Eric He',
+    description:
+      "Eric was BUGS's Secretary. Alumni from Stern who majored in Business and Mathematics and minored in Computer Science. He likes to play basketball, listens to music and read about philosophy.",
+  },
+];
